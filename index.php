7a90d5e4fd3c68db2fb78e7f4fbded1397db5d98
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-11 15:47:24 +0200

    Table inizializzata ma mi sto incasinando

--- index.php
+++ index.php
@@ -4,14 +4,38 @@ include 'data.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
 </head>
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        text-align: center;
+    }
+</style>
+
 <body>
 
-<h1><?php echo $prova; ?></h1>
-    
+    <h1><?php echo $prova; ?></h1>
+    <table style="width:100%">
+        <tr>
+            <th>name</th>
+            <th>description</th>
+            <th>parking</th>
+            <th>vote</th>
+            <th>distance to center</th>
+        </tr>
+        <tr>
+            <?php foreach ($hotels as $hotel) {
+                echo "<td>" . $hotel["name"] . "</td>";
+            }
+            ?>
+        </tr>
 </body>
+
 </html>
